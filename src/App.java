478e0a8648cada27bcd5f7aac1b8f5aee7b35b18
ThaisMK <thais.konishi@gmail.com>
2022-07-14 19:13:08 -0300

    inserindo dados

--- src/App.java
+++ src/App.java
@@ -1,5 +1,34 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+// A partir de uma lista de pessoas (nome e data nascimento no formato dd-MM-yy), calcular a idade de cada pessoa em referência a data de hoje.
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        List<Map<String, String>> listaPessoas = new ArrayList<>();
+        listaPessoas.add(Map.of("nome", "Joaquim", "nascimento", "11-12-1965"));
+        listaPessoas.add(Map.of("nome", "Maria", "nascimento", "24-07-1988"));
+        listaPessoas.add(Map.of("nome", "Caroline", "nascimento", "14-02-1993"));
+        listaPessoas.add(Map.of("nome", "Pedro Henrique", "nascimento", "02-11-1988"));
+
+        DateTimeFormatter padrao = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    }
+
+    public static Map<String, String> calculaIdade(List<Map<String, String>> listaPessoas, DateTimeFormatter padrao) {
+
+        LocalDate dataAtual, dataAComparar, idade;
+        dataAtual = LocalDate.now();
+        Map<String, String> listaPessoaIdade;
+        for (Map<String, String> dado : listaPessoas) {
+
+            dataAComparar = LocalDate.parse(dado.get("nascimento"), padrao);
+            Period period = Period.between(dataAComparar, dataAtual);
+
+        }
+        return null;
     }
 }
