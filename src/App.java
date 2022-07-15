08f312da798406d39b87163e9ea37897ce85ab84
ThaisMK <thais.konishi@gmail.com>
2022-07-15 16:21:06 -0300

    modificando a logica para entrada de anos de dois digitos

--- src/App.java
+++ src/App.java
@@ -1,20 +1,19 @@
 import java.time.LocalDate;
-import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-// A partir de uma lista de pessoas (nome e data nascimento no formato dd-MM-yy), calcular a idade de cada pessoa em referência a data de hoje.
 public class App {
     public static void main(String[] args) throws Exception {
         List<Map<String, String>> listaPessoas = new ArrayList<>();
-        listaPessoas.add(Map.of("nome", "Joaquim", "nascimento", "11-12-1965"));
-        listaPessoas.add(Map.of("nome", "Maria", "nascimento", "24-07-1988"));
-        listaPessoas.add(Map.of("nome", "Caroline", "nascimento", "14-02-1993"));
-        listaPessoas.add(Map.of("nome", "Pedro Henrique", "nascimento", "02-11-1988"));
+        listaPessoas.add(Map.of("nome", "Joaquim", "nascimento", "11-12-65"));
+        listaPessoas.add(Map.of("nome", "Maria", "nascimento", "24-07-88"));
+        listaPessoas.add(Map.of("nome", "Caroline", "nascimento", "14-02-93"));
+        listaPessoas.add(Map.of("nome", "Pedro Henrique", "nascimento", "02-11-88"));
+        listaPessoas.add(Map.of("nome", "Maria Antonieta", "nascimento", "02-11-15"));
 
-        DateTimeFormatter padrao = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        DateTimeFormatter padrao = DateTimeFormatter.ofPattern("dd-MM-yy");
 
         List<Map<String, String>> listaPessoaIdade = new ArrayList<>();
         listaPessoaIdade = calculaIdade(listaPessoas, padrao);
@@ -29,17 +28,33 @@ public class App {
     public static List<Map<String, String>> calculaIdade(List<Map<String, String>> listaPessoas,
             DateTimeFormatter padrao) {
 
-        LocalDate dataAtual, dataAComparar;
-        dataAtual = LocalDate.now();
+        LocalDate dataAComparar;
+        Integer anoAComparar, dataAtual;
+
         List<Map<String, String>> listaPessoaIdade = new ArrayList<>();
         for (Map<String, String> dado : listaPessoas) {
 
             dataAComparar = LocalDate.parse(dado.get("nascimento"), padrao);
-            Period idade = Period.between(dataAComparar, dataAtual);
-            String pessoa = dado.get("nome");
-            listaPessoaIdade.add(Map.of("nome", pessoa, "idade", String.format("%d", idade.getYears())));
+            anoAComparar = converteAno(dataAComparar.getYear());
+            dataAtual = LocalDate.now().getYear();
 
+            Integer idade = dataAtual - anoAComparar;
+            String pessoa = dado.get("nome");
+
+            listaPessoaIdade.add(Map.of("nome", pessoa, "idade", String.format("%d", idade)));
         }
+
         return listaPessoaIdade;
+    }
+
+    // converte ano de dois digitos para quatro digitos, pois java usa como default
+    // 2000 e não 1900 no getYear()
+    public static Integer converteAno(int ano) {
+        if (ano > 2023) {
+            return ano - 100;
+        } else {
+            return ano;
+        }
+
     }
 }
