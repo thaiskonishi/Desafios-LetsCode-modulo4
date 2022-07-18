2fd7fcae50ecd77eaf172cb1f14bf81285e03475
ThaisMK <thais.konishi@gmail.com>
2022-07-18 14:34:21 -0300

    alterando a taxa de gratificacao para torna-la dinamica

--- src/desafio2/Desafio2completo.java
+++ src/desafio2/Desafio2completo.java
@@ -47,83 +47,83 @@ public class Desafio2completo {
         String anoIntermediario = ano.concat("-01-01");
         LocalDate anoDate = LocalDate.parse(anoIntermediario, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
         DayOfWeek diaDaSemana = anoDate.getDayOfWeek();
-
+        Double taxaGratificacao = 0.05;
         Double gratificacaoSalario = (salario / 30.0);
 
         switch (diaDaSemana) {
             case MONDAY:
             case TUESDAY:
             case WEDNESDAY:
-                comissaoMensal.put(mes.JAN, (23.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.FEV, (20.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.MAR, (23.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.ABR, (22.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.MAI, (23.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.JUN, (22.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.JUL, (23.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.AGO, (23.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.SET, (22.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.OUT, (23.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.NOV, (22.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.DEZ, (23.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.JAN, (23.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.FEV, (20.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.MAR, (23.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.ABR, (22.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.MAI, (23.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.JUN, (22.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.JUL, (23.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.AGO, (23.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.SET, (22.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.OUT, (23.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.NOV, (22.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.DEZ, (23.0 * gratificacaoSalario * taxaGratificacao));
                 break;
 
             case THURSDAY:
-                comissaoMensal.put(mes.JAN, (22.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.FEV, (20.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.MAR, (22.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.ABR, (22.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.MAI, (22.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.JUN, (22.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.JUL, (22.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.AGO, (22.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.SET, (22.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.OUT, (22.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.NOV, (22.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.DEZ, (22.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.JAN, (22.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.FEV, (20.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.MAR, (22.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.ABR, (22.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.MAI, (22.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.JUN, (22.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.JUL, (22.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.AGO, (22.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.SET, (22.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.OUT, (22.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.NOV, (22.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.DEZ, (22.0 * gratificacaoSalario * taxaGratificacao));
                 break;
 
             case FRIDAY:
-                comissaoMensal.put(mes.JAN, (21.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.FEV, (20.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.MAR, (21.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.ABR, (21.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.MAI, (21.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.JUN, (21.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.JUL, (21.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.AGO, (21.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.SET, (21.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.OUT, (21.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.NOV, (21.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.DEZ, (21.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.JAN, (21.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.FEV, (20.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.MAR, (21.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.ABR, (21.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.MAI, (21.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.JUN, (21.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.JUL, (21.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.AGO, (21.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.SET, (21.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.OUT, (21.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.NOV, (21.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.DEZ, (21.0 * gratificacaoSalario * taxaGratificacao));
                 break;
             case SATURDAY:
-                comissaoMensal.put(mes.JAN, (21.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.FEV, (20.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.MAR, (21.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.ABR, (20.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.MAI, (21.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.JUN, (20.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.JUL, (21.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.AGO, (21.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.SET, (20.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.OUT, (21.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.NOV, (20.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.DEZ, (21.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.JAN, (21.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.FEV, (20.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.MAR, (21.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.ABR, (20.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.MAI, (21.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.JUN, (20.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.JUL, (21.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.AGO, (21.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.SET, (20.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.OUT, (21.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.NOV, (20.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.DEZ, (21.0 * gratificacaoSalario * taxaGratificacao));
                 break;
             case SUNDAY:
-                comissaoMensal.put(mes.JAN, (22.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.FEV, (20.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.MAR, (22.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.ABR, (21.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.MAI, (22.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.JUN, (21.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.JUL, (22.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.AGO, (22.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.SET, (21.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.OUT, (22.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.NOV, (21.0 * gratificacaoSalario * 0.005));
-                comissaoMensal.put(mes.DEZ, (22.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.JAN, (22.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.FEV, (20.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.MAR, (22.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.ABR, (21.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.MAI, (22.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.JUN, (21.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.JUL, (22.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.AGO, (22.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.SET, (21.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.OUT, (22.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.NOV, (21.0 * gratificacaoSalario * taxaGratificacao));
+                comissaoMensal.put(mes.DEZ, (22.0 * gratificacaoSalario * taxaGratificacao));
                 break;
         }
 
