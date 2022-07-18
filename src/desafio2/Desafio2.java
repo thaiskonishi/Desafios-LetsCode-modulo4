2ea1358e6d9627381ec64aca4f3f1f0ffca40fc5
ThaisMK <thais.konishi@gmail.com>
2022-07-18 14:30:29 -0300

    usando EnumMap para devolver a comissao mes a mes

--- src/desafio2/Desafio2.java
+++ src/desafio2/Desafio2.java
@@ -1,16 +1,105 @@
 package desafio2;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Desafio2 {
 
-    public static void main(String[] args) {
+    public enum mes {
+        JAN, FEV, MAR, ABR, MAI, JUN, JUL, AGO, SET, OUT, NOV, DEZ
+    }
 
-        String ano = "2022";
+    public static void getComissaoMensal(Double salario, String ano) {
+
         String anoIntermediario = ano.concat("-01-01");
         LocalDate anoDate = LocalDate.parse(anoIntermediario, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        System.out.println(anoDate.getDayOfWeek());
+        DayOfWeek diaDaSemana = anoDate.getDayOfWeek();
+
+        Double gratificacaoSalario = (salario / 30.0);
+
+        switch (diaDaSemana) {
+            case MONDAY:
+            case TUESDAY:
+            case WEDNESDAY:
+                System.out.println(mes.JAN + "  " + (23.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.FEV + "  " + (20.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.MAR + "  " + (23.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.ABR + "  " + (22.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.MAI + "  " + (23.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.JUN + "  " + (22.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.JUL + "  " + (23.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.AGO + "  " + (23.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.SET + "  " + (22.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.OUT + "  " + (23.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.NOV + "  " + (22.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.DEZ + "  " + (23.0 * gratificacaoSalario * 0.005));
+                break;
+
+            case THURSDAY:
+                System.out.println(mes.JAN + "  " + (22.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.FEV + "  " + (20.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.MAR + "  " + (22.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.ABR + "  " + (22.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.MAI + "  " + (22.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.JUN + "  " + (22.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.JUL + "  " + (22.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.AGO + "  " + (22.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.SET + "  " + (22.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.OUT + "  " + (22.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.NOV + "  " + (22.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.DEZ + "  " + (22.0 * gratificacaoSalario * 0.005));
+                break;
+
+            case FRIDAY:
+                System.out.println(mes.JAN + "  " + (21.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.FEV + "  " + (20.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.MAR + "  " + (21.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.ABR + "  " + (21.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.MAI + "  " + (21.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.JUN + "  " + (21.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.JUL + "  " + (21.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.AGO + "  " + (21.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.SET + "  " + (21.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.OUT + "  " + (21.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.NOV + "  " + (21.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.DEZ + "  " + (21.0 * gratificacaoSalario * 0.005));
+                break;
+            case SATURDAY:
+                System.out.println(mes.JAN + "  " + (21.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.FEV + "  " + (20.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.MAR + "  " + (21.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.ABR + "  " + (20.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.MAI + "  " + (21.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.JUN + "  " + (20.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.JUL + "  " + (21.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.AGO + "  " + (21.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.SET + "  " + (20.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.OUT + "  " + (21.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.NOV + "  " + (20.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.DEZ + "  " + (21.0 * gratificacaoSalario * 0.005));
+                break;
+            case SUNDAY:
+                System.out.println(mes.JAN + "  " + (22.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.FEV + "  " + (20.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.MAR + "  " + (22.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.ABR + "  " + (21.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.MAI + "  " + (22.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.JUN + "  " + (21.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.JUL + "  " + (22.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.AGO + "  " + (22.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.SET + "  " + (21.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.OUT + "  " + (22.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.NOV + "  " + (21.0 * gratificacaoSalario * 0.005));
+                System.out.println(mes.DEZ + "  " + (22.0 * gratificacaoSalario * 0.005));
+                break;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        getComissaoMensal(1200.0, "2022");
 
     }
 }
---
+++ src/desafio2/Desafio2completo.java
@@ -0,0 +1,139 @@
+package desafio2;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.EnumMap;
+import java.util.Map;
+
+public class Desafio2completo {
+    private String nome;
+    private Double salario;
+    public EnumMap<mes, Double> comissaoMensal;
+
+    public Desafio2completo(String nome, Double salario) {
+        this.nome = nome;
+        this.salario = salario;
+        this.comissaoMensal = new EnumMap<>(mes.class);
+
+    }
+
+    public enum mes {
+        JAN, FEV, MAR, ABR, MAI, JUN, JUL, AGO, SET, OUT, NOV, DEZ
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
+    public Map<mes, Double> getComissaoMensal() {
+        return comissaoMensal;
+    }
+
+    public Map<mes, Double> getComissao(Double salario, String ano) {
+
+        String anoIntermediario = ano.concat("-01-01");
+        LocalDate anoDate = LocalDate.parse(anoIntermediario, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        DayOfWeek diaDaSemana = anoDate.getDayOfWeek();
+
+        Double gratificacaoSalario = (salario / 30.0);
+
+        switch (diaDaSemana) {
+            case MONDAY:
+            case TUESDAY:
+            case WEDNESDAY:
+                comissaoMensal.put(mes.JAN, (23.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.FEV, (20.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.MAR, (23.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.ABR, (22.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.MAI, (23.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.JUN, (22.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.JUL, (23.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.AGO, (23.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.SET, (22.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.OUT, (23.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.NOV, (22.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.DEZ, (23.0 * gratificacaoSalario * 0.005));
+                break;
+
+            case THURSDAY:
+                comissaoMensal.put(mes.JAN, (22.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.FEV, (20.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.MAR, (22.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.ABR, (22.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.MAI, (22.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.JUN, (22.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.JUL, (22.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.AGO, (22.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.SET, (22.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.OUT, (22.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.NOV, (22.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.DEZ, (22.0 * gratificacaoSalario * 0.005));
+                break;
+
+            case FRIDAY:
+                comissaoMensal.put(mes.JAN, (21.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.FEV, (20.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.MAR, (21.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.ABR, (21.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.MAI, (21.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.JUN, (21.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.JUL, (21.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.AGO, (21.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.SET, (21.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.OUT, (21.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.NOV, (21.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.DEZ, (21.0 * gratificacaoSalario * 0.005));
+                break;
+            case SATURDAY:
+                comissaoMensal.put(mes.JAN, (21.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.FEV, (20.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.MAR, (21.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.ABR, (20.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.MAI, (21.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.JUN, (20.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.JUL, (21.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.AGO, (21.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.SET, (20.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.OUT, (21.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.NOV, (20.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.DEZ, (21.0 * gratificacaoSalario * 0.005));
+                break;
+            case SUNDAY:
+                comissaoMensal.put(mes.JAN, (22.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.FEV, (20.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.MAR, (22.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.ABR, (21.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.MAI, (22.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.JUN, (21.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.JUL, (22.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.AGO, (22.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.SET, (21.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.OUT, (22.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.NOV, (21.0 * gratificacaoSalario * 0.005));
+                comissaoMensal.put(mes.DEZ, (22.0 * gratificacaoSalario * 0.005));
+                break;
+        }
+
+        return comissaoMensal;
+    }
+
+    public static void main(String[] args) {
+        Desafio2completo pablo = new Desafio2completo("Pablo", 1000.0);
+        pablo.getComissao(12000.0, "2022");
+        System.out.println("Gratificacao mes a mes " + pablo.getComissaoMensal());
+
+    }
+}
